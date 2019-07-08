a32f7a00877c3fffc35b8e476f0ac54e26dad59f
Maya Bose <47787632+boseHere@users.noreply.github.com>
2019-07-08 10:31:07 -0700

    Add files via upload

--- scripts/index_genomes.sh
+++ scripts/index_genomes.sh
@@ -1,7 +1,17 @@
-for file in ./../data/genomes/*/*.fasta
+#!/bin/bash
+for file in data/genomes/*/
 do
+num_files=`ls -l $file | grep ".ebwt$" | wc -l`
+
+if [ $num_files -lt 6 ]
+then
+new_file=$file*.fasta
 substring=$(basename $file)
 subdir=${file%"$substring"}
-bowtie-build -f $file ${substring:0:-6} &&
-mv ./*ebwt $subdir
+echo $substring
+echo $subdir
+bowtie-build -f $new_file ${substring} &&
+mv ./$substring*.ebwt $subdir
+fi
+
 done
